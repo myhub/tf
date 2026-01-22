8a0d97c85122e47f625ad49216795d476fad0180
myhub <3228648623@qq.com>
2026-01-22 11:46:38 +0800

    add

--- nlp_a60.py
+++ nlp_a60.py
@@ -5,26 +5,22 @@ from torch.utils.data import Dataset
 import sys
 from io import StringIO
 
-TEST_SIZE = 10000
+TEST_SIZE = 100000
 
-class NlpM3(Dataset): 
+class NlpA60(Dataset): 
     def __init__(self, is_training=True):
-   
-        self.alphabet = [' ', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '*', "="]
+        
+        self.alphabet = [' ', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '+', "="]
 
-        self.seq_len = 20
-        self.aout_len = 8
+        self.seq_len = 192
+        self.aout_len = 64
+
         self.is_training = is_training
-
+        
         self.char2idx = {}
         for i, c in enumerate(self.alphabet):
             self.char2idx[c] = i
 
-        random.seed(2026)
-
-        self.idx_list = list(range(1000000))
-        random.shuffle(self.idx_list)
-        
     def __len__(self):
         if self.is_training:
             return 2048 * 8
@@ -32,19 +28,22 @@ class NlpM3(Dataset):
             return TEST_SIZE
     
     def __getitem__(self, index):
-        if self.is_training:
-            _ = index
-            idx = self.idx_list[random.randrange(len(self.idx_list) - TEST_SIZE)]
-        else:
-            assert index < TEST_SIZE
-            idx = self.idx_list[len(self.idx_list) - TEST_SIZE + index]
+        # 因为数据集规模足够大，测试集出现在训练集的概率极低，所以此处不区分训练集/测试集
+        _ = index
         
-        m1 = idx % 1000
-        m2 = idx // 1000
+        m1 = random.randrange(10**60)
+        m2 = random.randrange(10**60)
+        is_add = random.random() < 0.5
 
-        ain = f"{m1}*{m2}=        "
-        aout = f"{m1*m2}"
+        if is_add:
+            ain = f"{m1}+{m2}="
+            aout = f"{m1+m2}"
+        else:
+            ain = f"{m1}-{m2}="
+            aout = f"{m1-m2}"
         
+        ain += ' '*self.aout_len
+
         ain = [0]*(self.seq_len - len(ain)) + [self.char2idx[c] for c in ain] 
         aout = [0]*(self.aout_len - len(aout)) + [self.char2idx[c] for c in aout]
         
@@ -55,16 +54,18 @@ class NlpM3(Dataset):
 
 
 if __name__ == '__main__':
-    ds = NlpM3(is_training=False)
+    ds = NlpA60(is_training=False)
 
     for i in range(len(ds)):
         ain, aout = ds[i]
         print(' | ', end="")
         for idx in ain:
             c = ds.alphabet[idx]
+            if c == ' ': continue
             print(c, end='')
         print(' | ', end="")
         for idx in aout:
             c = ds.alphabet[idx]
+            if c == ' ': continue
             print(c, end='')
         print(' | ')
